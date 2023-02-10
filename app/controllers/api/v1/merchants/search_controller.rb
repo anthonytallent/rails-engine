461dfa44c9ff421d919fa6f1d965439aa0b65a17
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-02-09 16:39:18 -0800

    Fix typo

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::Merchants::SearchController < ApplicationController
     if params[:name].present? == false
       render json: "", status: 400
     elsif params[:name].empty?
-      render json: "", status: 400›
+      render json: "", status: 400
     elsif params[:name]
       # if Merchant.where("name ILIKE ?", "%#{params[:name]}%").sort.first == nil
       #   render json: { data: { error: "Merchant not found" } }, status: 200
